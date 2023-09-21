940a0a0a19cb0e633f725e954aa41c518ef45e9d
ksh <eric3941@naver.com>
2023-09-21 19:54:31 +0900

    test activeuser1

--- room.go
+++ room.go
@@ -8,28 +8,15 @@ import (
 )
 
 type room struct {
-
-	// forward is a channel that holds incoming messages
-	// that should be forwarded to the other clients.
 	forward chan []byte
-
-	// join is a channel for clients wishing to join the room.
-	join chan *client
-
-	// leave is a channel for clients wishing to leave the room.
-	leave chan *client
-
-	// clients holds all current clients in this room.
+	join    chan *client
+	leave   chan *client
 	clients map[*client]bool
-
-	// tracer will receive trace information of activity
-	// in the room.
 }
 
-// newRoom makes a new room that is ready to
-// go.
 func newRoom() *room {
 	return &room{
+
 		forward: make(chan []byte),
 		join:    make(chan *client),
 		leave:   make(chan *client),
@@ -41,17 +28,16 @@ func (r *room) run() {
 	for {
 		select {
 		case client := <-r.join:
-			// joining
+
 			r.clients[client] = true
 
 		case client := <-r.leave:
-			// leaving
+
 			delete(r.clients, client)
 			close(client.send)
 
 		case msg := <-r.forward:
 
-			// forward message to all clients
 			for client := range r.clients {
 				log.Printf("Sending message to client: %v", client)
 				client.send <- msg
@@ -66,7 +52,12 @@ const (
 	messageBufferSize = 256
 )
 
-var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize, CheckOrigin: func(r *http.Request) bool { return true }}
+var upgrader = &websocket.Upgrader{
+
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
