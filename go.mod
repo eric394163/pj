e8748d052f2470f941e140d446eed133927e221c
ksh <eric3941@naver.com>
2023-10-04 23:03:54 +0900

    231004

--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/gorilla/websocket v1.5.0
 )
 
-require golang.org/x/crypto v0.13.0 // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
-	"strconv"
 	"sync"
 	"text/template"
 
 	"gopjex/dbcon"
 
 	"github.com/gorilla/websocket"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 var mainRoom = newRoom()
@@ -49,101 +44,6 @@ func DBConnection() (*dbcon.DBConnection, error) {
 	return dbc, nil
 }
 
-func Register(w http.ResponseWriter, r *http.Request, dbc *dbcon.DBConnection) {
-	r.ParseForm()
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	username := r.FormValue("id")
-	password := r.FormValue("password")
-
-	stmt, err := dbc.Conn.Prepare("INSERT INTO users (name, email, username, password, is_admin) VALUES (?, ?, ?, ?, ?)")
-	if err != nil {
-		log.Printf("Failed to prepare statement: %v", err)
-		http.Error(w, "Failed to register", http.StatusInternalServerError)
-		return
-	}
-	defer stmt.Close()
-
-	// 비밀번호 해싱
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Printf("Failed to hash password: %v", err)
-		http.Error(w, "Failed to register", http.StatusInternalServerError)
-		return
-	}
-	// 해시된 비밀번호를 데이터베이스에 저장
-	_, err = stmt.Exec(name, email, username, string(hashedPassword), 0) //0 은 일반 유저
-	if err != nil {
-		log.Printf("Failed to insert into database: %v", err)
-		http.Error(w, "Failed to register", http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
-}
-
-func Login(w http.ResponseWriter, r *http.Request, dbc *dbcon.DBConnection) {
-	r.ParseForm()
-	username := r.FormValue("id")
-	password := r.FormValue("password")
-
-	stmt, err := dbc.Conn.Prepare("SELECT password, name, email, is_admin FROM users WHERE username=?")
-	if err != nil {
-		log.Printf("Failed to prepare statement: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	defer stmt.Close()
-
-	var storedPassword, name, email string
-	var isAdmin bool
-	err = stmt.QueryRow(username).Scan(&storedPassword, &name, &email, &isAdmin)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
-			return
-		}
-		log.Printf("Failed to get user data: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(password))
-	if err != nil {
-		http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
-		return
-	}
-
-	// 로그인 성공 하면 쿠키 설정
-
-	authCookie := &http.Cookie{
-		Name:  "auth",
-		Value: username,
-		Path:  "/",
-	}
-	http.SetCookie(w, authCookie)
-	nameCookie := &http.Cookie{
-		Name:  "name",
-		Value: url.QueryEscape(name),
-		Path:  "/",
-	}
-	http.SetCookie(w, nameCookie)
-	emailCookie := &http.Cookie{
-		Name:  "email",
-		Value: url.QueryEscape(email),
-		Path:  "/",
-	}
-	http.SetCookie(w, emailCookie)
-	adminCookie := &http.Cookie{
-		Name:  "isAdmin",
-		Value: strconv.FormatBool(isAdmin), // bool 값을 string으로 변환해서 저장
-		Path:  "/",
-	}
-	http.SetCookie(w, adminCookie)
-
-	// chat.html 리다이렉트
-	http.Redirect(w, r, "/chat", http.StatusSeeOther)
-}
-
 func handleChat(w http.ResponseWriter, r *http.Request) {
 	if websocket.IsWebSocketUpgrade(r) {
 		if !ismainRoomRunning {
@@ -171,7 +71,6 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.Handle("/login", &templateHandler{filename: "login/login.html"})
 	http.Handle("/register", &templateHandler{filename: "register/register.html"})
-
 	http.HandleFunc("/processRegister", func(w http.ResponseWriter, r *http.Request) {
 		Register(w, r, dbc)
 	})
